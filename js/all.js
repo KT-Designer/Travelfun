fa8585f87c6d68c6cb01615ff71a47c25102ecc9
Doris <130676335+KT-Designer@users.noreply.github.com>
2023-11-02 17:38:32 +0800

    Add files via upload

---
+++ js/all.js
@@ -0,0 +1,64 @@
+// Lottie
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('scroll_down'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../images/scroll_down.json'
+})
+
+
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('swipe_up'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../images/swipe_up.json'
+})
+
+
+
+
+
+
+
+
+
+
+
+// 浪漫歐洲
+$(document).ready(function () {
+    // 初始化 Slider
+    $('.slider').each(function () {
+        var $slider = $(this);
+        var $nav = $slider.find('.slider-nav .icon');
+        var $slides = $slider.find('.slider-content .slide');
+
+        // 當點選 ICON 時切換 Slide
+        $nav.click(function () {
+            var slideIndex = $(this).data('slide');
+            $slides.hide();
+            $slides.filter('[data-slide="' + slideIndex + '"]').show();
+        });
+
+        // 預設顯示第一張 Slide
+        $nav.first().click();
+    });
+});
+
+
+// 浪漫歐洲_點選時維持特效
+const icons = document.querySelectorAll('.icon');
+
+// 將點擊事件添加到每個圖片上
+icons.forEach(icon => {
+    icon.addEventListener('click', () => {
+        // 移除所有圖片的 active 類別
+        icons.forEach(icon => icon.classList.remove('active'));
+        // 將被點擊的圖片添加 active 類別，改變濾鏡效果
+        icon.classList.add('active');
+    });
+});
+
+
---
+++ js/scrolltrigger.js
@@ -0,0 +1,116 @@
+// KV滾動視差
+gsap.registerPlugin(ScrollTrigger);
+const t2 = gsap.timeline({
+    scrollTrigger: {
+    },
+});
+t2.to("#scroll_down", { opacity: 0 });
+
+
+const tl = gsap.timeline({
+    scrollTrigger: {
+        // 滾動.kv時觸發
+        trigger: ".kv",
+        // 固定
+        pin: true,
+        // 滾動一次的執行速度
+        scrub: 0.5,
+    },
+});
+tl.to("#left", { xPercent: "60" });
+tl.to(".airplane", { x: "40vw", y: "-30vh" }, "<");
+tl.to("#word", { yPercent: "-60" }, "<");
+tl.to("#right", { xPercent: "-60" }, "<");
+
+
+
+
+
+
+
+
+
+// 淡進
+gsap.registerPlugin(ScrollTrigger);
+let mm = gsap.matchMedia();
+mm.add("(min-width: 1200px)", () => {
+    // 熱門景點
+    const fadein1 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".hot",
+            start: "top 40%",
+            end: "bottom 50%",
+        }
+
+    });
+    fadein1.from('#L1', { opacity: 0, x: -100, duration: 0.8 })
+    fadein1.from('#L2', { opacity: 0, x: -100, duration: 0.8 })
+    fadein1.from('#R1', { opacity: 0, x: 100, duration: 0.8 })
+
+
+    // 浪漫歐洲
+    const fadein2 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".europe",
+            start: "top top",
+            end: "bottom 50%",
+        }
+
+    });
+    fadein2.from('#L3', { opacity: 0, x: -100, duration: 0.8 })
+    fadein2.from('#R2', { opacity: 0, x: 100, duration: 0.8 })
+
+
+    // 中東古文明
+    const fadein3 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".turkey",
+            start: "top 20%",
+            end: "bottom 50%",
+        }
+
+    });
+    fadein3.from('#L4', { opacity: 0, x: -100, duration: 0.8 })
+    fadein3.from('#R3', { opacity: 0, x: 100, duration: 0.8 })
+
+    // 紐澳仙境
+    const fadein4 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".australia",
+            start: "top top",
+            end: "bottom 50%",
+        }
+
+    });
+    fadein4.from('#R4', { opacity: 0, x: 100, duration: 0.8 })
+    fadein4.from('#L5', { opacity: 0, x: -100, duration: 0.8 })
+
+    // 北美奇觀
+    const fadein5 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".america",
+            start: "top top",
+            end: "bottom 50%",
+        }
+
+    });
+    fadein5.from('#L6', { opacity: 0, x: -100, duration: 0.8 })
+    fadein5.from('#R5', { opacity: 0, x: 100, duration: 0.8 })
+
+    return () => {
+    };
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
